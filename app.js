a9d52397680216fbbc081aa2eaf43367531c51f0
mackmcgovern <mkrmcgovern@gmail.com>
2024-05-15 09:48:44 +0700

    fix scroll animations

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
 // Define global variables
-const navList = document.querySelector(".nav-list");
+const navList = document.querySelector('.nav-list')
 
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section')
 
 // Create the nav links
 const makeNav = () => {
-  let navUl = "";
-  sections.forEach((section) => {
-    const sectionData = section.dataset.nav;
-    const sectionId = section.id;
+  let navUl = ''
+  sections.forEach(section => {
+    const sectionData = section.dataset.nav
+    const sectionId = section.id
 
-    navUl += `<li><a href="#${sectionId}" class="nav-link ${sectionId}" data-nav="${sectionId}">${sectionData}</a></li>`;
-  });
+    navUl += `<li><a href="#${sectionId}" class="nav-link ${sectionId}" data-nav="${sectionId}">${sectionData}</a></li>`
+  })
 
-  navList.innerHTML = navUl;
-};
+  navList.innerHTML = navUl
+}
 
-makeNav();
+makeNav()
 
 // Add smooth scroll function
-const smoothScroll = (e) => {
-  e.preventDefault();
+const smoothScroll = e => {
+  e.preventDefault()
   const scrollSect = document.getElementById(
-    e.currentTarget.getAttribute("data-nav")
-  );
+    e.currentTarget.getAttribute('data-nav')
+  )
 
   scrollSect.scrollIntoView({
-    behavior: "smooth",
-    inline: "center",
-  });
-};
+    behavior: 'smooth',
+    inline: 'center',
+  })
+}
 
 document
-  .querySelectorAll(".nav-link")
-  .forEach((link) => link.addEventListener("click", smoothScroll));
+  .querySelectorAll('.nav-link')
+  .forEach(link => link.addEventListener('click', smoothScroll))
 
 // Add active class to section when viewing
-window.addEventListener("scroll", () => {
-  let current = "";
-  const navLinks = document.querySelectorAll("a.nav-link");
+window.addEventListener('scroll', () => {
+  let current = ''
+  const navLinks = document.querySelectorAll('a.nav-link')
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    if (scrollY >= sectionTop - 70) {
-      current = section.getAttribute("id");
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop
+    if (scrollY >= sectionTop - 83) {
+      current = section.getAttribute('id')
     }
-  });
-  sections.forEach((section) => {
+  })
+  sections.forEach(section => {
     if (section.classList.contains(current)) {
-      section.classList.add("active");
+      section.classList.add('active')
     } else {
-      section.classList.remove("active");
+      section.classList.remove('active')
     }
-  });
-  navLinks.forEach((navLink) => {
+  })
+  navLinks.forEach(navLink => {
     if (navLink.classList.contains(current)) {
-      navLink.classList.add("active");
+      navLink.classList.add('active')
     } else {
-      navLink.classList.remove("active");
+      navLink.classList.remove('active')
     }
-  });
-});
+  })
+})
